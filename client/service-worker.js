6d55177eb08cafdced0b9d548012d895298072be
eustatos <astaskhinav@gmail.com>
2018-11-27 06:45:43 +0400

    refactor(pwa): fix audit

--- client/service-worker.js
+++ client/service-worker.js
@@ -20,3 +20,24 @@ self.addEventListener('push', function(event) {
     })
   );
 });
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open('v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/app.js',
+        '/manifest.json'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
